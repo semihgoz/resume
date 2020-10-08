775859738ab51fedf5e90cdd8d2b5197ea003cc7
semih.goz <semih.goz@etiya.com>
2020-10-08 11:17:12 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Semih Göz, Resume
+
+[http://semihgoz61.github.io/resume](http://semihgoz61.github.io/resume)
---
+++ src/metadata/metadata.js
@@ -0,0 +1,118 @@
+module.exports = {
+  name: 'Semih Göz',
+  title: 'Back-End Developer (Spring EJB)',
+  facts: {
+    Residence: 'Bayrampaşa/İstanbul',
+    Homepage: `<a href="https://github.com/semihgoz61">
+                  <span class="print">https://github.com/semihgoz61</span>
+                  <span class="screen">https://github.com/semihgoz61</span>
+                </a>`,
+    Email: '<a href="mailto:semihgoz61@gmail.com">semihgoz61@gmail.com</a>',
+    LinkedIn: '<a href="https://www.linkedin.com/in/semihgoz/">https://www.linkedin.com/in/semihgoz/</a>',
+    Phone: '553 862 43 61',
+  },
+  skills: [
+    {
+      title: 'General',
+      props: ['Core Java', 'Spring', 'Hibernate'],
+    },
+    {
+      title: 'Technical',
+      props: [
+        'Design pattern from GOF',
+        'Exception Handling RFC-7807',
+        'OOP with Java',
+        'Layered Architecture',
+        'SSO with OAuth2',
+      ],
+    },
+    {
+      title: 'Back-End Advanced',
+      props: [
+        'Spring Boot',
+        'Spring Freamwork 5',
+        'Hibernate Native or JPA',
+        'RDBMS (Posgresql, MariaDb)',
+        'NoSQL (Redis)',
+        'JCache Hazelcast, Redis, Ehcache',
+        'Spring Boot Starters Data-Rest, Security, Validation, OpenApiDoc (Swagger3), Hateoas, etc.',
+        'Lombok',
+        'Junit (5)',
+        'Serializations Jackson, Gson, Kryo',
+        'Rest-Communucation ApacheHttpClient, RestTemplate (From AppacheHttpClient), Retrofit (From Okhttp), Feign (With ApacheHttpClient)',
+      ],
+    },
+    {
+      title: 'Back-End Intermediate',
+      props: [
+        'Spring Cloud Eureka(Name-Server), Config-Server, Admin (With actuator), Hystirx, Feign, Spring-Gateway or Zool',
+        'Keycloak',
+      ],
+    },
+    {
+      title: 'Tools',
+      props: ['Git', 'Maven', 'Jenkins', 'Docker'],
+    },
+  ],
+  workHistory: [
+    {
+      title: 'Back-End Developer',
+      company: 'Etiya Bilgi Teknolojileri, İstanbul',
+      date: {
+        start: 'Oct 2019',
+        end: 'Current',
+      },
+      contents: [
+        {
+          title: 'Marvel Project, Back-End Developer (https://videotron.com/en)',
+          props: [
+            'I am learning to work with a team that offers corporate projects and solutions',
+            'I try to improve the existing complex project with existing competencies.',
+            'We make improvements on compact projects that produce large systems and permanent solutions, such as Keycloak, Amazon WSO2, etc.',
+            'Integration of different projects and we make improvements suitable for this integration, for example payment system (i2i), SPA etc.',
+            'We learn to adapt to any unit quickly by supporting different teams.',
+            'We learn the concept of Continuous Integration and Delivery',
+          ],
+        },
+      ],
+      badges: ['Core Java', 'JPA/Hiberante', 'Spring Freamwork', 'Spring Boot'],
+    },
+    {
+      title: 'Back-End Developer',
+      company: 'UMB Mühendislik',
+      date: {
+        start: 'June 2017',
+        end: 'Oct 2019',
+      },
+      contents: [
+        {
+          title: 'UMB mühendislik spolia project from nodeJs to Spring',
+          props: [
+            'The company I joined to transition from NodeJs to the spring ecosystem',
+            'I did work that I thought I did good work on spring with the responsibility I took by acquiring introductory level spring knowledge.',
+            'Develop better solutions with design patterns in recent times',
+            'I met microservice architecture',
+          ],
+        },
+      ],
+      badges: ['Core Java', 'JPA/Hiberante', 'Spring Freamwork', 'Spring Boot', 'NodeJs', 'Spring Cloud'],
+    },
+  ],
+  educations: [
+    {
+      title: 'Bachelor of Science: Software Engineer',
+      description: 'Beykent University - Istanbul',
+      date: {
+        start: 'Sep 2012',
+        end: 'Jul 2018',
+      },
+    },
+  ],
+  affiliations: [
+    'Erhan Günal, Software Team Leader, 0 507 614 09 64',
+    'Sonay Ergünlü, Senior Software Developer (Back-End) 0 537 682 93 53',
+    'Recep Şengüler, Senior Software Developer (Back-End) 0 537 927 51 72',
+    'Samet Gündoğmuş, Senior Software Developer (Mobile) 0 537 216 94 81',
+  ],
+  additional: ['Military Services: Completed', 'No smoking'],
+};
